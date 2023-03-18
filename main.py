37524741b18993c0124b5774b92998344e665afd
User <Ptetents@foo.ru>
2023-03-18 19:26:50 +0700

    калькулятоор

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-dayNumber = int(input("введите число: "))#День недели в виде числа
-result = dayNumber >= 1 and dayNumber <= 7
-
-if result == False:
-    print("Не правильный день недели. Программа закрывается...")
-    exit()
-
-dayOfWeek = "none"
-
-if dayNumber == 1:
-    dayOfWeek = "Понедельник"
-elif dayNumber == 2:
-    dayOfWeek = "вторник"
-elif dayNumber == 3:
-    dayOfWeek = "средат"
-elif dayNumber == 4:
-    dayOfWeek = "четверг"
-elif dayNumber == 5:
-    dayOfWeek = "пятница"
-elif dayNumber == 6:
-    dayOfWeek = "суббота"
-elif dayNumber == 7:
-    dayOfWeek = "воскресенье"  
-
-print(dayOfWeek)
+a = int(input("Ввдетие А: "))
+b = int(input("Ввдетие B: "))
+operation = input("Введите знак операции: ")
+#/ // % * - +
+if operation == "+":
+    print(f"A {operation} B = {a + b}")
+elif operation == "-":
+    print(f"A {operation} B = {a - b}")
+elif operation == "*":
+    print(f"A {operation} B = {a * b}")
+elif operation == "/":
+    print(f"A {operation} B = {a / b}")
+elif operation == "%":
+    print(f"A {operation} B = {a % b}")
